74fcdd244154647f989d264f6c786e8c5bbb10d1
riisatoro <riisa.toro@gmail.com>
2021-04-10 17:00:41 +0300

    Added models for the game and rooms

--- server/models/models.py
+++ server/models/models.py
@@ -1,4 +1,13 @@
-from django.db.models import EmailField, DateTimeField
+from django.db.models import (
+    Model,
+    EmailField,
+    DateTimeField,
+    BooleanField,
+    JSONField,
+    ManyToManyField,
+    ForeignKey,
+    CASCADE
+)
 from django.contrib.auth.models import AbstractUser
 
 
@@ -9,3 +18,23 @@ class CustomUser(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+
+class Rooms(Model):
+    player = ForeignKey(CustomUser, on_delete=CASCADE)
+    game = ForeignKey("Game", on_delete=CASCADE)
+    player_cards = JSONField()
+
+
+class Game(Model):
+    is_started = BooleanField(default=False)
+    is_ended = BooleanField(default=False)
+    last_card = JSONField(null=True)
+    players = ManyToManyField(CustomUser, through=Rooms)
+    started_at = DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{is_started}?{is_ended}?{started_at}"
+
+
+
