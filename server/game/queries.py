849dad1004307fb4c53eb85555849b1f4738f370
riisatoro <riisa.toro@gmail.com>
2021-05-30 12:01:02 +0300

    Finished join and left the game

--- server/game/queries.py
+++ server/game/queries.py
@@ -1,23 +1,40 @@
 from models.models import Rooms, Game
 
 
-def is_player_in_room(users:list):
-    return Game.objects.filter(is_ended=False, players__in=users).exists()
+def is_player_in_room(users: list):
+    return Game.objects.filter(
+        is_ended=False,
+        players__in=users,
+        rooms__has_left=False,
+    ).exists()
 
 
 def get_available_games():
-    return Game.objects.filter(
-        is_started=False, is_ended=False
-    ).all()
+    return Game.objects.filter(is_started=False, is_ended=False).all()
+
 
 def get_game_by_id(id):
     return Game.objects.filter(id=id).first()
 
+
 def get_room_by_game_player(game, player):
     return Rooms.objects.filter(game=game, player=player).first()
 
+
 def count_players_in_game(game):
-    game = Game.objects.filter(id=game.id).first()
+    game = Game.objects.filter(id=game.id, rooms__has_left=False).first()
     if not game:
         return 0
     return game.players.count()
+
+
+def join_player_game(player, game, cards):
+    return Rooms(player=player, game=game, player_cards=cards).save()
+
+
+def player_allowed_to_join_game(player, game_id):
+    return (
+        Game.objects.filter(id=game_id, is_started=False, is_ended=False)
+        .exclude(players__in=[player])
+        .exists()
+    )
--- server/game/views.py
+++ server/game/views.py
@@ -1,11 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.status import (
-    HTTP_200_OK,
-    HTTP_201_CREATED,
-    HTTP_400_BAD_REQUEST
-)
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from game.forms import GameCreateForm, RoomCreateForm
 from game.queries import (
     is_player_in_room,
@@ -13,36 +9,48 @@ from game.queries import (
     get_game_by_id,
     get_room_by_game_player,
     count_players_in_game,
+    join_player_game,
+    player_allowed_to_join_game,
 )
 from gamecore.main import get_random_card
 from serializers.serializers import AvailableGamesSerializer
 
 
 class GameView(APIView):
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [
+        IsAuthenticated,
+    ]
 
     def get(self, request):
-        ''' Receive available games to join'''
+        """Receive available games to join"""
         games = get_available_games()
         data = AvailableGamesSerializer(games, many=True).data
         return Response(data, status=HTTP_200_OK)
 
     def put(self, request):
-        '''Join existed game'''
+        """Join existed game"""
         if is_player_in_room([request.user]):
             return Response(
-                {'details': 'You are allowed to play only in one room'},
-                status=HTTP_400_BAD_REQUEST
+                {"details": "You are allowed to play only in one room"},
+                status=HTTP_400_BAD_REQUEST,
             )
-        game = get_game_by_id(request.POST.get('game_id'))
-        return Response()
+        if not player_allowed_to_join_game(request.user, request.POST.get("game_id")):
+            return Response({"details": "You are not allowed to join this game"})
+
+        game = get_game_by_id(request.POST.get("game_id"))
+        cards = get_random_card()
+        join_player_game(request.user, game, cards)
+        if game.players.count() == game.player_amount:
+            game.is_started=True
+            game.save()
+        return Response({}, status=HTTP_200_OK)
 
     def post(self, request):
-        ''' Create own game '''
+        """Create own game"""
         if is_player_in_room([request.user]):
             return Response(
-                {'details': 'You are allowed to play only in one room'},
-                status=HTTP_400_BAD_REQUEST
+                {"details": "You are allowed to play only in one room"},
+                status=HTTP_400_BAD_REQUEST,
             )
 
         new_game = GameCreateForm(request.POST)
@@ -50,34 +58,37 @@ class GameView(APIView):
             return Response(new_game.errors.as_data(), status=HTTP_400_BAD_REQUEST)
 
         new_game = new_game.save()
-        new_room = RoomCreateForm({
-            'player': request.user,
-            'game': new_game,
-            'player_cards': get_random_card(),
-        })
+        new_room = RoomCreateForm(
+            {
+                "player": request.user,
+                "game": new_game,
+                "player_cards": get_random_card(),
+            }
+        )
         new_room.save()
         return Response({}, status=HTTP_201_CREATED)
 
     def delete(self, request):
-        game = get_game_by_id(request.POST.get('game_id'))
+        game = get_game_by_id(request.POST.get("game_id"))
         room = get_room_by_game_player(game, request.user)
         if not game or not room:
-            return Response({'details': 'This game or room does not exist'})
+            return Response({"details": "This game or room does not exist"})
 
-        room.delete()
-        if count_players_in_game(game) == 1:
+        room.has_left = True
+        room.save()
+        if count_players_in_game(game) < 2:
             game.is_ended = True
             game.save()
         return Response({}, status=HTTP_200_OK)
 
 
 class ResultsView(APIView):
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [
+        IsAuthenticated,
+    ]
 
     def get(self, request):
         data = ResultSerializer(
-            Rooms.objects.filter(game__is_ended=True),
-            many=True).data
-        return Response(
-            data, status=HTTP_200_OK
-        )
+            Rooms.objects.filter(game__is_ended=True), many=True
+        ).data
+        return Response(data, status=HTTP_200_OK)
