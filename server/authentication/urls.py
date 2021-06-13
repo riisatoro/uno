9df263d402995c91b764855d072dce7bddd3d6fd
AswillHatter <oboroten.ler@gmail.com>
2021-06-13 10:19:29 +0300

    Get username from server

--- server/authentication/urls.py
+++ server/authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-from .views import Registration
+from .views import Registration, UserData
 
 urlpatterns = [
     path('register/', Registration.as_view(), name='registration'),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='get_token'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='refresh_token'),
+    path('user/', UserData.as_view(), name='user_data'),
 ]
--- server/authentication/views.py
+++ server/authentication/views.py
@@ -5,6 +5,8 @@ from rest_framework.status import (
     HTTP_201_CREATED,
     HTTP_400_BAD_REQUEST
 )
+from rest_framework.permissions import IsAuthenticated
+from serializers.serializers import CustomUserSerializer
 
 from .forms import UserCreationForm
 
@@ -16,3 +18,15 @@ class Registration(APIView):
             user.save()
             return Response({}, status=HTTP_200_OK)
         return Response(user.errors.as_data(), status=HTTP_400_BAD_REQUEST)
+
+
+class UserData(APIView):
+    permission_classes = [
+        IsAuthenticated,
+    ]
+
+    def get(self, request):
+        user = request.user
+        data = CustomUserSerializer(user).data
+        return Response(data, HTTP_200_OK)
+
